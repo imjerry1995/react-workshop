4c2280c194504c4b14fa31b45916f24aa795daef
Jerry Weng <dragonryu09@gmail.com>
2020-03-30 14:34:21 +0800

    fix bugs

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 
 const Item  = props => {
-    const item = {...props}
+    const {number2, handleAdd2, handleMinus2} = props
     return (
         <div className="item">
-          <p>奶茶 數量: {item.number2}</p>
-          <button onClick={item.handleAdd2}>+</button>
-          <button onClick={item.handleMinus2}>-</button>
+          <p>奶茶 數量: {number2}</p>
+          <button onClick={handleAdd2}>+</button>
+          <button onClick={handleMinus2}>-</button>
         </div>
     )
 }
