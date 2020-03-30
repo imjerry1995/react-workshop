8d01437dc82c588429243d4c22ed3dc35b481158
Jerry Weng <dragonryu09@gmail.com>
2020-03-30 14:15:25 +0800

    add pdf

--- src/components/Done.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-const Done = props => {
-    return (
-        <button className="done" onClick={props.handleSubmit}>OK</button>
-    )
-}
-
-export default Done
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // 引入 react
-import ReactDOM from 'react-dom'; // 引入 react-dom
-import App from './App'; // 引入 App 元件
-
-
-ReactDOM.render( <App />,document.getElementById('root'));
-// 透過 React-dom 將 App 這個元件渲染到 html 檔中 id 為 root 的節點
-
